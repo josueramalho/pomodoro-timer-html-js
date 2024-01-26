93ca7c62f1576f5305d0c3f47d27b7556bf9938b
Josué Ramalho <josuermlh@gmail.com>
2024-01-26 04:34:05 -0300

    Added toogle music function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,29 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const title = document.querySelector('.app__title')
+const buttons = document.querySelectorAll('.app__card-button')
+const musicInput = document.querySelector('#alternar-musica')
+const musica = new Audio('../assets/sounds/luna-rise-part-one.mp3')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 }) 
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    buttons.forEach(contexto => {
+        contexto.classList.remove('active')
+    });
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `../assets/img/${contexto}.png`)
     switch (contexto) {
@@ -35,3 +44,12 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+musicInput.addEventListener('change', () => {
+    musica.loop = true
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
