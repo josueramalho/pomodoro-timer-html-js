4037f733f4df8737383320ded9bb54c2efb069f0
Josué Ramalho <josuermlh@gmail.com>
2024-01-30 13:51:37 -0300

    Added countdown functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,18 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const startPauseBt = document.querySelector('#start-pause')
 const banner = document.querySelector('.app__image')
 const title = document.querySelector('.app__title')
 const buttons = document.querySelectorAll('.app__card-button')
 const musicInput = document.querySelector('#alternar-musica')
 const musica = new Audio('../assets/sounds/luna-rise-part-one.mp3')
+const soundForPlay = new Audio('../assets/sounds/play.wav')
+const soundForPause = new Audio('../assets/sounds/pause.mp3')
+const soundWhenFinish = new Audio('../assets/sounds/beep.mp3')
+
+let tempOnSec = 50
+let intervalId = null
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -53,3 +60,31 @@ musicInput.addEventListener('change', () => {
         musica.pause()
     }
 })
+
+const contRegressive = () => {
+    if (tempOnSec <= 0) {
+        console.log(intervalId)
+        soundWhenFinish.play()
+        clearCont()
+        return
+    }
+    tempOnSec -= 1
+    console.log('Temporizador ' + tempOnSec)
+}
+
+startPauseBt.addEventListener('click', startPauseCont)
+
+function startPauseCont() {
+    if (intervalId) {
+        soundForPause.play()
+        clearCont()
+        return
+    }
+    soundForPlay.play()
+    intervalId = setInterval(contRegressive, 1000)
+}
+
+function clearCont() {
+    clearInterval(intervalId)
+    intervalId = null
+}
